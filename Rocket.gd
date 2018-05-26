65819cea3f784d43ae32009ced9b9a7a23205900
Marcin Muskala <marcin.muskala@gmail.com>
2018-05-26 09:40:26 +0200

    rocket impl

--- Rocket.gd
+++ Rocket.gd
@@ -1,19 +1,25 @@
 extends KinematicBody2D
 
 const SPEED = 150
+const ROT = 0.5
 
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
 	var direction = Vector2()
+	print(self.position.y)
 	if Input.is_action_pressed("ui_down"):
-		direction.y += SPEED
+		if self.position.x > -140:
+			if self.rotation < 0.4:
+				self.rotation += ROT * delta
+			direction.y += SPEED
 	if Input.is_action_pressed("ui_up"):
-		direction.y += -SPEED
-	if Input.is_action_pressed("ui_left"):
-		direction.x += -SPEED
-	if Input.is_action_pressed("ui_right"):
-		direction.x += SPEED
+		if self.position.x < 140:
+			if self.rotation > -0.4:
+				self.rotation -= ROT * delta
+			direction.y += -SPEED
+	if Input.is_action_just_released("ui_down") or Input.is_action_just_released("ui_up"):
+		self.rotation = 0
 
 	set_position(self.position + direction.normalized() * SPEED * delta)
